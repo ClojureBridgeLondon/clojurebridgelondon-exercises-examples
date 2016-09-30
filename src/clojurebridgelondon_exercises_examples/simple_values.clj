155c297e973278e5e8f150c940fe74d75fb31ccf
John Stevenson <john@jr0cket.co.uk>
2016-09-30 10:56:47 +0100

    Added eariler examples in simple values section
    Examples created in previous ClojureBridge events

--- src/clojurebridgelondon_exercises_examples/simple_values.clj
+++ src/clojurebridgelondon_exercises_examples/simple_values.clj
@@ -17,7 +17,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;; To go a little further... we bind names to the values of hours and minutes
-
 (def hours 2)
 (def minutes 5)
 
@@ -44,8 +43,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;; or writing this using the threading macro
 (-> minutes
-    (+ (* hours 60))
-    (* 60))
+    (+ ,,, (* hours 60))
+    (* ,,, 60))
 
 
 
@@ -67,3 +66,84 @@
 
 (str "There are " (quot 1000 60) " minutes and " (rem 1000 60) " seconds.")
 ;; => "There are 16 minutes and 40 seconds."
+
+;; (/ 1000 60)
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Simple values from old ClojureBridge curriculum
+
+;;;;;;;;;;;;
+;; simple values
+
+"This is a simple string"
+"Strings evaluate to themselves"
+"Strings are immutable like other data structures in Clojure"
+(str "Strings" "can" "be" "joined" "togehter" "using" "the" "str" "function")
+(str "but str doesnt add" "spaces" "between strings")
+(str "so add your own" " " "spaces")
+
+
+;; Experimenting with joining strings
+(def join-my-strings-with-spaces ["join" "us" "toether" "with" "spaces"])
+
+;; (map str join-my-strings-with-spaces)
+
+;; (apply str join-my-strings-with-spaces)
+
+;; Using another function called interpose, we can place something between each string as we join them together
+(apply str (interpose " " join-my-strings-with-spaces))
+
+;; See https://clojuredocs.org/clojure.core/interpose
+
+
+;; If you come from a Java world, you may want to use println to output a string.  Well you can
+
+(println "This is my string, but caveat emptor, this is a side effect")
+
+;; When you evaluate the above, you do not get a string in return... so what is going on?
+
+;; Discuss evalation verses side effects here?
+
+
+;; EXERCISE: Store the name of your hometown
+
+"Ripon"
+
+;; Write the name of your hometown as a string, and then assign that string to the name my-hometown.
+;; EXERCISE: Make a function to format names
+
+(def my-hometown "Ripon")
+
+my-hometown
+
+;; The str function can take any number of arguments, and it concatenates them together to make a string. Write a function called format-name that takes two arguments, first-name and last-name. This function should output the name like so: Last, First.
+
+(defn format-name
+  "Concatenate two strings together"
+  [firstname lastname]
+  (str firstname " " lastname))
+
+(format-name "John" "Stevenson")
+
+;; Adding in our interpose example from before...
+
+(defn format-name
+  "Concatenate two strings together"
+  [firstname lastname]
+  (interpose " " (str firstname lastname)))
+
+(format-name "John" "Stevenson")
+
+;; Thats not quite right.  If we change the arguments to a vector then we can
+
+(defn format-name
+  "Concatenate two strings together"
+  [strings-to-join]
+  (apply str (interpose " " strings-to-join)))
+
+(format-name ["John" "Stevenson"])
+
